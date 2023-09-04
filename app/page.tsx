347cf45703c183e6913def1f1ad0e9dd6f44e155
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2023-09-04 10:56:10 +0700

    First Commit

---
+++ app/page.tsx
@@ -0,0 +1,104 @@
+import {
+  IconBrandInstagram,
+  IconBrandFacebook,
+  IconBrandLinkedin,
+} from "@tabler/icons-react";
+import ProjectCard from "../components/ProjectCard";
+import BlogImage from "../public/blog-homepage.png";
+import BushiImage from "../public/bushi-homepage.png";
+import RancangImage from "../public/rancang-creative.png";
+
+export default function Home() {
+  return (
+    <div>
+      {/* Banner */}
+
+      <div
+        id="top"
+        className="h-screen flex flex-col items-center justify-center"
+      >
+        <h1 className="text-9xl mb-5 text-center text-yellow-400">
+          Nizar Baihaqi
+        </h1>
+        <h2 className="text-2xl mb-5 font-medium">Web Developer</h2>
+        <p>Scroll down</p>
+      </div>
+
+      {/* My Projects */}
+
+      <h1
+        id="projects"
+        className="text-4xl text-center mb-5 font-bold scroll-smooth scroll-mt-14"
+      >
+        My Projects
+      </h1>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+        <ProjectCard
+          title="My Blog Post"
+          desc="I post my assignment here 😂"
+          link="https://blog.nizarbaihaqi.com"
+          img={BlogImage}
+          clicked={false}
+        />
+        <ProjectCard
+          title="Bushi Computer"
+          desc="A computer repair shop"
+          link="https://bushi.nizarbaihaqi.com"
+          img={BushiImage}
+          clicked={true}
+        />
+        <ProjectCard
+          title="Rancang Creative"
+          desc="Social Media Agency"
+          link="https://rancang-creative.vercel.app"
+          img={RancangImage}
+          clicked={false}
+        />
+      </div>
+
+      {/* Contacts */}
+
+      <div className="mt-10">
+        <h1 className="text-4xl text-center mb-5 font-bold">How to hire me</h1>
+        <h3 className="text-lg text-center">
+          You can hire me by sending an email to{` `}
+          <a
+            href="mailto:nizarbaihaq@gmail.com"
+            className="p-2 hover:bg-cyan-600 border border-white rounded transition-all"
+          >
+            nizarbaihaq@gmail.com
+          </a>
+        </h3>
+      </div>
+
+      {/* Social Media */}
+
+      <div className="mt-5 mb-10 gap-3 flex justify-center">
+        <a
+          href="https://instagram.com/ijaybaihaqi"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="p-2 hover:bg-cyan-600 rounded"
+        >
+          <IconBrandInstagram stroke={1.5} />
+        </a>
+        <a
+          href="https://www.facebook.com/nizar.baihaqi.94/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="p-2 hover:bg-cyan-600 rounded"
+        >
+          <IconBrandFacebook stroke={1.5} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/nizar-baihaqi/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="p-2 hover:bg-cyan-600 rounded"
+        >
+          <IconBrandLinkedin stroke={1.5} />
+        </a>
+      </div>
+    </div>
+  );
+}
---
+++ components/ProjectCard.tsx
@@ -0,0 +1,34 @@
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+
+export type ProjectCardProp = {
+  title: string;
+  desc: string;
+  link: string;
+  img: StaticImageData;
+  clicked: boolean;
+};
+
+export default function ProjectCard({
+  title,
+  desc,
+  link,
+  img,
+  clicked,
+}: ProjectCardProp) {
+  return (
+    <a
+      href={link}
+      target="_blank"
+      className="relative h-96 border-y md:border border-white max-w-screen-md md:w-[720px] lg:w-full md:mx-auto md:rounded-lg"
+    >
+      <div className="absolute inset-0 -z-10">
+        <Image src={img} alt="Project Image" fill objectFit="cover" />
+      </div>
+      <div className="absolute inset-0 bg-black/80 lg::opacity-0 hover:opacity-100 transition-all flex flex-col justify-center items-center hover:underline">
+        <h2 className="text-2xl mb-3 font-medium">{title}</h2>
+        <p>{desc}</p>
+      </div>
+    </a>
+  );
+}
